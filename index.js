4fe27e1a633b4bb7fbcbd7a619d63ced47ddd3d8
Jakub Bełtowski <jbeltowski@griddynamics.com>
2025-12-18 15:31:47 +0100

    fix: bug fix with dotenv

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import express from "express";
 import cors from "cors";
 import mongoose from "mongoose";
-import "dotenv/config.js";
+import "dotenv/config";
 import { fileURLToPath } from "url";
 import { dirname } from "path";
 
